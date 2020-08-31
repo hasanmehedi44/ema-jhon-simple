d1a634c53fbd0ea21a5fdd525d1c2e0e03452cd6
hasanmehedi44 <67532498+hasanmehedi44@users.noreply.github.com>
2020-09-01 01:45:20 +0600

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import logo from '../../images/logo.png';
 import './Header.css';
@@ -9,7 +10,7 @@ const Header = () => {
             <nav>
                 <a href="/shop">Shop</a>
                 <a href="/review">Order Review</a>
-                <a href="/manage">Manage Inventory</a>
+                <a href="/inventory">Manage Inventory</a>
             </nav>
         </div>
     );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,20 @@
+
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import "./Product.css";
+import { Link } from "react-router-dom";
 
 const Product = (props) => {
-  const { img, name, seller, price, stock } = props.product;
+  const { img, name, seller, price, stock, key } = props.product;
+  console.log(props.product);
   return (
     <div className="product">
       <div>
         <img src={img}></img>
       </div>
       <div>
-        <h4 className="product-name">{name}</h4>
+        <h4 className="product-name"><Link to={"/product/"+key}>{name}</Link></h4>
         <br />
         <p>
           <small>by: {seller} </small>
